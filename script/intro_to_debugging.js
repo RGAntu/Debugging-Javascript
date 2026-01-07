179713723e0608155d951cbc136fee333713bc8b
RGAntu <rgantu887@gmail.com>
2026-01-08 00:27:16 +0600

    added reference error

--- script/intro_to_debugging.js
+++ script/intro_to_debugging.js
@@ -35,12 +35,40 @@
 // 01. Incorrect data types in operation
 
 let number = 42;
-number() // Trying to call a number as a function 
+// number() // Trying to call a number as a function 
 
 let user;
-console.log(user.name) // user is undefined
+// console.log(user.name) // user is undefined
 
 
 // 02. Calling a method on incorrect object type
 let number1 = 42; 
-number1.toUpperCase(); // TypeError: number.toUpperCase is not a function
+// number1.toUpperCase(); // TypeError: number.toUpperCase is not a function
+
+
+
+// <------------------ 3. Reference Error -----------> 
+const sum = () => {
+    const result = 10 + 22;
+}
+
+console.log(result);
+
+// Example 1 : Undeclared variable 
+console.log(x); // ReferenceError: x is not defined
+
+
+// Example 2: Variable out of scope
+function myFunction(){
+    let y = 5;
+}
+console.log(y) // ReferenceError: y is not defined
+
+
+
+/**
+ * ------ Common Causes ----- 
+ * 1. Undeclared variable: Using a variable without declaring it with let, const or var.
+ * 
+ * 2. Variable out of scope: Trying to access a variable from outside the block where it was declared (like using a variable defined inside a function outside of it).
+ */
