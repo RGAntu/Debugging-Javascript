d3db8afd5521d22533e22d9a527e5df1cea4d295
RGAntu <rgantu887@gmail.com>
2026-01-08 02:10:48 +0600

    added debug txt & try js

---
+++ script/try.js
@@ -0,0 +1,19 @@
+
+const name = "ant";
+try{
+console.log("code is working");
+// console.log(num);
+if(name === "antu"){
+    console.log("wow you are", name);
+}
+else{
+    throw "You are not I am looking for"
+}
+}
+catch(error){
+    console.log(error);
+}
+finally{
+    console.log(" code still working");
+
+}
