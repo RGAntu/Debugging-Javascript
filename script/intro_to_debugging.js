cf90ad09de84a4d7cb955de105eb7e986adeae48
RGAntu <rgantu887@gmail.com>
2026-01-08 00:07:26 +0600

    added syntax errors

--- script/intro_to_debugging.js
+++ script/intro_to_debugging.js
@@ -4,3 +4,16 @@
 // let x = "Redoy ghosh
 console.log(x)
 
+// 02 -> Unmatched Parentheses 
+// console.log( "hello, redoy" 
+
+
+// 03 -> Incorrect Keyword 
+for (Let i = 0; i < 5; i++){
+    console.log(i)
+}
+
+// 04 -> Unexpected Token
+const for = 0;
+
+// 05 ->  Unexpected End
